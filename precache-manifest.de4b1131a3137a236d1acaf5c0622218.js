bf04a3388cb281b0343b19932cabffc640a60803
unknown <brnthmilk@gmail.com>
2023-05-28 09:40:25 +0900

    1st commit

---
+++ precache-manifest.de4b1131a3137a236d1acaf5c0622218.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75cacf0eb4e1f4938122e8c142d41a58",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1e3273d845d4bc267e94",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "690d4be715b01f04a411",
+    "url": "/static/js/2.c634e52c.chunk.js"
+  },
+  {
+    "revision": "86abd83cec51454159bb68109fd2739c",
+    "url": "/static/js/2.c634e52c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e3273d845d4bc267e94",
+    "url": "/static/js/main.ffb16715.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.ffb16715.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7b7595fe6bd528f00e7",
+    "url": "/static/js/runtime-main.24d62bfc.js"
+  }
+]);
